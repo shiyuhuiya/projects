24a601c73ccd6464603cb7137b5bf0eb86981835
syhy <3519450570@qq.com>
2025-02-19 22:51:36 +0800

    修改wallpaper资源路径错误

---
+++ docs/wallpaper/assets/user.Bk7mJtIJ.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index.DwUfxuiK.js";const a=()=>e({url:"/userInfo"}),p=({type:a,pageNum:p,pageSize:r})=>e({url:"/userWallList",data:{type:a,pageNum:p,pageSize:r}});export{p as a,a as g};
