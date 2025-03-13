fabb3649f4001f578fc20d6c658e220157c5c693
syhy <3519450570@qq.com>
2025-03-13 20:48:31 +0800

    修正资源路径错误

---
+++ docs/rabbit/assets/castArray-DuUOOmyL.js
@@ -0,0 +1,1 @@
+import{bg as a}from"./index-BOcrrdDX.js";function e(){if(!arguments.length)return[];var r=arguments[0];return a(r)?r:[r]}export{e as c};
---
+++ docs/rabbit/assets/pay-QTrODOhH.js
@@ -0,0 +1,1 @@
+import{an as e}from"./index-BOcrrdDX.js";const o=r=>e.get(`/member/order/${r}`);export{o as g};
